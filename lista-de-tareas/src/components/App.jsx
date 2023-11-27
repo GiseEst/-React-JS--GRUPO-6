ff37e855aaf647248cb53d2e1cae596b62963235
Giseest <gisella.estrada2@gmail.com>
2023-11-27 01:29:43 -0300

    seg comm

--- lista-de-tareas/src/components/App.jsx
+++ lista-de-tareas/src/components/App.jsx
@@ -1,3 +1,4 @@
+/*POR SI LAS DUDAS lo dejo en comentario hasta que se termine
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,6 +34,66 @@ function App() {
 }
 
 export default App
+*/
+
+import React, { useState, useEffect } from 'react';
+import { TaskForm } from './components/TaskForm';
+import { TaskList } from './components/TaskList';
+import './style/style.css'
+
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  // Funciones para gestionar eventos
+  const handleComplete = taskId => {
+    // Lógica para marcar como completada una tarea
+    // Actualización del estado de las tareas
+    const updatedTasks = tasks.map(task =>
+      task.id === taskId ? { ...task, completed : !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const handleDelete = taskId => {
+    // Lógica para eliminar una tarea
+    // Actualización del estado de las tareas
+    const updatedTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  const addTask = taskName => {
+    // Lógica para agregar una nueva tarea
+    // Generación de una nueva tarea y actualización del estado de las tareas
+    const newTask = {
+      id: tasks.length + 1,
+      name: taskName,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  // useEffect para realizar acciones cuando cambia el estado de las tareas
+  useEffect(() => {
+    // Ejemplo: Mostrar un mensaje cuando se agrega o elimina una tarea
+    console.log('Tareas actualizadas:', tasks);
+  }, [tasks]);
+
+  return (
+    <div>
+      <h1>App - Lista de Tareas</h1>
+      <TaskForm addTask={addTask} />
+      <TaskList
+        tasks={tasks}
+        handleComplete={handleComplete}
+        handleDelete={handleDelete}
+      />
+    </div>
+  );
+};
+
+export default App;
+
 // iconos para agregar : import { MdOutlineTaskAlt } from "react-icons/md"; import { BsListTask } from "react-icons/bs";
 //iconos animados https://www.flaticon.es/iconos-animados-mas-descargados
 // gif para la parte de abajo: <iframe src="https://gifer.com/embed/1PwK" width=480 height=360.241 frameBorder="0" allowFullScreen></iframe><p><a href="https://gifer.com">a través de GIFER</a></p>
