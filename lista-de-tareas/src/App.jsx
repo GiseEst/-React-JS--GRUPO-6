ff5c642182447f9fe6cff5ebd94e3262d3da3ceb
Giseest <gisella.estrada2@gmail.com>
2023-11-26 21:36:19 -0300

    add: style

--- lista-de-tareas/src/App.jsx
+++ lista-de-tareas/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './style.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -33,3 +33,10 @@ function App() {
 }
 
 export default App
+// iconos para agregar : import { MdOutlineTaskAlt } from "react-icons/md"; import { BsListTask } from "react-icons/bs";
+//iconos animados https://www.flaticon.es/iconos-animados-mas-descargados
+// gif para la parte de abajo: <iframe src="https://gifer.com/embed/1PwK" width=480 height=360.241 frameBorder="0" allowFullScreen></iframe><p><a href="https://gifer.com">a través de GIFER</a></p>
+// gif para tema 1 <iframe src="https://gifer.com/embed/14Se" width=480 height=480.000 frameBorder="0" allowFullScreen></iframe><p><a href="https://gifer.com">a través de GIFER</a></p>
+// gif para tema 2 <iframe src="https://gifer.com/embed/6mz" width=480 height=480.000 frameBorder="0" allowFullScreen></iframe><p><a href="https://gifer.com">a través de GIFER</a></p>
+// gif para tema 3 <iframe src="https://gifer.com/embed/14Su" width=480 height=480.000 frameBorder="0" allowFullScreen></iframe><p><a href="https://gifer.com">a través de GIFER</a></p>
+// gif para tema 4 <iframe src="https://gifer.com/embed/JQo" width=480 height=480.000 frameBorder="0" allowFullScreen></iframe><p><a href="https://gifer.com">a través de GIFER</a></p>
